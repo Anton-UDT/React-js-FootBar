8da70482b8de72349aea7ec54dc5fa7b4eca5bca
Anton-UDT <lolseedanton@gmail.com>
2018-07-07 12:56:01 +0100

    Index page

---
+++ index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Link from 'next/link';
+import "../styles/styles.scss";
+import FootBar from '../components/footBarComp';
+
+export default class extends Component {
+    render(){
+        return(
+            <div>
+                    <h3>This is the index</h3>
+                    <FootBar isIndex nextPage="page-2" />
+            </div>
+        );
+    }
+}
+
