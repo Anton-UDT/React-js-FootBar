6ee6cd47328ba8ad924f482c30f5b39ebf57a189
Anton-UDT <lolseedanton@gmail.com>
2018-07-07 13:02:51 +0100

    Page-2 with links to previous and next

--- page-2.js
+++ page-2.js
@@ -7,7 +7,7 @@ export default class extends Component {
     render(){
         return(
             <div>
-                    <h3>This is the index</h3>
+                    <h3>This is the second page</h3>
                     <FootBar isPage2 prevPage="index" nextPage="page-3" />
             </div>
         );
