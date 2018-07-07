f82fbd3641ef8f843f4ad3c9bbdf424e08f464cb
Anton-UDT <lolseedanton@gmail.com>
2018-07-07 13:30:51 +0100

    Component for the foot-bar

--- footBarComp.js
+++ footBarComp.js
@@ -4,9 +4,8 @@ import Link from 'next/link';
 function footerChecker(props) {
     // Setting prop variables
     const isIndex = props.isIndex;
-    const isSlideshow = props.isSlideshow;
-    const isGallery = props.isGallery;
-    const isArtwork = props.isArtwork;
+    const isPage2 = props.isPage2;
+    const isPage3 = props.isPage3;
 
     //Check to see if component has 'isIndex'
     if(isIndex) {
@@ -20,8 +19,8 @@ function footerChecker(props) {
                     </Link>
                 </div>;
     }
-    //Check to see if component has 'isSlideshow'
-    else if(isSlideshow) {
+    //Check to see if component has 'isPage2'
+    else if(isPage2) {
         return <div className="container-fluid border-top border-primary margin-top fixed-bottom back-red bg-white">
                     <Link href={props.prevPage}>
                     <div className="d-inline-block pr-3 float-left">
@@ -39,24 +38,14 @@ function footerChecker(props) {
                     </Link>
                 </div>;
     }
-    //Check to see if component has 'isArtwork'
-    else if(isArtwork) {
+    else if(isPage3) {
         return <div className="container-fluid border-top border-primary margin-top fixed-bottom back-red bg-white">
                     <Link href={props.prevPage}>
                     <div className="d-inline-block pr-3 float-left">
                         <div className="btn btn-primary btn-circle">
                             <i className="fas fa-arrow-left"></i>
                         </div>
-                    </div>
-                    </Link>
-                    <Link href={props.nextPage}>
-                        <div className="d-inline-block pr-3 float-right">
-                            <div className="btn btn-primary btn-circle">
-                                <i className="fas fa-arrow-right"></i>
-                            </div>
-                        </div>
-                    </Link>
-                </div>;
+                    </div>;
     }
     //Set default
     else {
